60a106803c94726d9ea69a9d0f95e985527786c3
YR200310 <ryoyama000000@gmail.com>
2024-08-03 20:01:50 +0900

    product.k

---
+++ client/src/pages/Product.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import reactLogo from '../assets/react.svg';  // パスを修正
+
+function Product() {
+  return (
+    <>
+    <div>
+    </div>
+    <div className="flex">
+    <h2 className="text-2xl w-1/4">商品名</h2>
+    <h2 className="text-2xl">レビュー</h2>
+    </div>
+    <div class="flex justify-end">
+    <div class="w-1/2 h-96 overflow-y-auto border border-gray-300">
+      <div class="flex-none x-10 space-y-4">
+        <div className="border border-green-300 border-double p-4">
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        </div>
+        <div className="border border-green-300 border-double p-4">
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        </div>
+        <div className="border border-green-300 border-double p-4">
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        </div>
+        
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+        <div class="text-xl text-center">
+          ★★★★★　疲れた
+        </div>
+        <div class="text-lg">
+          昨日の午後、友人とカフェでゆっくり過ごしました。美味しいコーヒーとケーキを楽しみながら、楽しい会話に花が咲きました。リラックスした時間がとても心地よかったです。
+        </div>
+
+      </div>
+    </div>
+</div>
+
+    </>
+  );
+}
+
+export default Product;
