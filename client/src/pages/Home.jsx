f66962a3f2620871131467da7804584407d97db4
tayuuki <yuuki.tatsu848@gmail.com>
2024-08-04 04:53:42 +0900

    バックと画面遷移の接続2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <body className='bg-gray-100'>
-                <div className="flex flex-grow px-10 mx-auto w-full">
+            <body className='min-h-screen bg-gray-100'>
+                <div className="flex px-10 mx-auto w-full">
 
-                    <section className="flex">
-                        <div className="border-solid border-8 md:">
+                    <section className="flex mt-8">
+                        <div className="border-solid border-8">
                             <div className="font-medium text-xl px-20 pt-5 text-center md:text-left md:text-xl">
                                 今のおすすめ
                             </div>
@@ -19,54 +19,93 @@ const Home = () => {
                                 <li className='list-none'><a href="#">商品名</a></li>
                                 <li className='list-none'><a href="#">商品名</a></li>
                                 <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                                    Button
+                                    もっと見る＞
                                 </button>
                             </nav>
                         </div>
                     </section>
 
-                    <div className="flex flex-col justify-end mx-auto overflow-x-auto">
+                    <div className="flex flex-col justify-end mx-auto mt-8">
                         <section className="w-full p-4 border-8 border-solid border-blue-500">
                             <div className="flex gap-4 justify-center">
                                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20 object-cover rounded-full" />
+                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20" />
+                                    
                                 </a>
                                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20 object-cover rounded-full" />
+                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20" />
                                 </a>
                                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20 object-cover rounded-full" />
+                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20" />
                                 </a>
                                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20 object-cover rounded-full" />
+                                    <img src="./img/mac.jpg" alt="a" className="w-20 h-20" />
                                 </a>
                                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-                                    <img src="./img/mac.jpg" alt="b" className="w-20 h-20 object-cover rounded-full" />
+                                    <img src="./img/mac.jpg" alt="b" className="w-20 h-20" />
                                 </a>
                             </div>
                         </section>
-                        <section className="p-4">
-                            <ul className="flex flex-wrap border-solid border-8 justify-center h-80 overflow-y-auto">
-                                <li className="basis-2/5 text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
+                        <section className="p-4 mt-8">
+                            <ul className="w-full grid grid-cols-1 gap-4 border-solid border-8 border-teal-400 h-96 overflow-y-auto">
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
                                     <a href="https://example.com">
-                                        <img src="./img/mac.jpg" alt="mac" className="w-28 mx-auto" />
+                                        レビュー
                                     </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
                                 </li>
-                                <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
                                     <a href="https://example.com">
-                                        <img src="./img/mac.jpg" alt="mac" className="w-28 mx-auto" />
+                                        レビュー
                                     </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
                                 </li>
-                                <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
                                     <a href="https://example.com">
-                                        <img src="./img/mac.jpg" alt="mac" className="w-28 mx-auto" />
+                                        レビュー
                                     </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
                                 </li>
-                                <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
                                     <a href="https://example.com">
-                                        <img src="./img/mac.jpg" alt="mac" className="w-28 mx-auto" />
+                                        レビュー
                                     </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
                                 </li>
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
+                                    <a href="https://example.com">
+                                        レビュー
+                                    </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
+                                </li>
+                                <li className="flex flex-col text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl h-60">
+                                    <a href="https://example.com">
+                                        レビュー
+                                    </a>
+                                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-auto">
+                                        もっと見る＞
+                                    </button>
+
+                                </li>
+
+
+                               
+                               
                             </ul>
                         </section>
 
--- flask-server/item.sql
+++ flask-server/item.sql
@@ -4,6 +4,25 @@
 -- 表示モードをboxに変更
 .mode box
 
+-- reviewsテーブルを削除（存在する場合）
+DROP TABLE IF EXISTS reviews;
+
+-- reviewsテーブルを削除（存在する場合）
+DROP TABLE IF EXISTS reviews;
+-- reviewsテーブルを作成
+CREATE TABLE reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    company TEXT,
+    comment TEXT,
+    rate DOUBLE
+);
+
+INSERT INTO reviews (name, company, comment, rate)
+VALUES ('すいかフラッペ', 'マクドナルド', 'センスないわマック', 5.0);
+
+SELECT * FROM reviews;
+
 -- itemsテーブルを削除（存在する場合）
 DROP TABLE IF EXISTS items;
 
@@ -17,19 +36,44 @@ CREATE TABLE items (
     finish DATE,
     image_path VARCHAR(255) NOT NULL, 
     rate DOUBLE,
-    intro TEXT
+    intro TEXT,
+    rev_num INTEGER
 );
 
 -- itemsテーブルにデータを挿入
-INSERT INTO items (company, name, price, start, finish, image_path, rate,intro) 
-VALUES ('test', 'test item', '400', '2024-01-01', '2025-01-01', './img/test.jpg', 5, 'これはテストです');
-INSERT INTO items (company, name, price, start, finish, image_path, rate,intro) 
-VALUES ('mac', 'すいかフラッペ', '490~', '2024-07-31', '2024-08-31', './img/suika.jpg', 5, 'すいか果汁を使用したスムージーベースに、ホイップクリームとキウイ＆ばななソースをトッピングしました。
-すいかをそのままかじったような、見た目にも楽しいすっきりした甘さのフラッペです。');
-INSERT INTO items (company, name, price, start, finish, image_path, rate,intro) 
-VALUES ('mac', 'ゴールデンパインフラッペ', '490~', '2024-07-31', '2024-08-31', './img/pain.jpg', 5, 'ゴールデンパイン果汁を使用したスムージーベースに、ホイップクリームとゴールデンパインソースをトッピングした、トロピカルな香りと爽やかでジューシーな甘さのフラッペです 。');
-INSERT INTO items (company, name, price, start, finish, image_path, rate,intro) 
-VALUES ('mac', 'マカロン ばなな', '190~', '2024-07-31', '2025-01-01', './img/banana.jpg', 5, 'アーモンド入りのサクふわ生地に、 バナナピューレを配合したやさしい甘さが特長のクリームをサンドしたまろやかな味わいのマカロン。');
+INSERT INTO items (company, name, price, start, finish, image_path, rate,intro, rev_num) 
+VALUES ('test', 'test item', '400', '2024-01-01', '2025-01-01', './img/test.jpg', 5, 'これはテストです', (SELECT COUNT(*) FROM reviews WHERE reviews.name = 'test'));
+INSERT INTO items (company, name, price, start, finish, image_path, rate,intro, rev_num) 
+VALUES ('マクドナルド', 'すいかフラッペ', '490~', '2024-07-31', '2024-08-31', './img/suika.jpg', 5, 'すいか果汁を使用したスムージーベースに、ホイップクリームとキウイ＆ばななソースをトッピングしました。
+すいかをそのままかじったような、見た目にも楽しいすっきりした甘さのフラッペです。', (SELECT COUNT(*) FROM reviews WHERE reviews.name = 'すいかフラッペ'));
+INSERT INTO items (company, name, price, start, finish, image_path, rate,intro, rev_num) 
+VALUES ('マクドナルド', 'ゴールデンパインフラッペ', '490~', '2024-07-31', '2024-08-31', './img/pain.jpg', 5, 'ゴールデンパイン果汁を使用したスムージーベースに、ホイップクリームとゴールデンパインソースをトッピングした、トロピカルな香りと爽やかでジューシーな甘さのフラッペです 。', (SELECT COUNT(*) FROM reviews WHERE reviews.name = 'ゴールデンパインフラッペ'));
+INSERT INTO items (company, name, price, start, finish, image_path, rate,intro, rev_num) 
+VALUES ('マクドナルド', 'マカロン ばなな', '190~', '2024-07-31', '2025-01-01', './img/banana.jpg', 5, 'アーモンド入りのサクふわ生地に、 バナナピューレを配合したやさしい甘さが特長のクリームをサンドしたまろやかな味わいのマカロン。', (SELECT COUNT(*) FROM reviews WHERE reviews.name = 'マカロン ばなな'));
 
 -- itemsテーブルの内容を表示
 SELECT * FROM items;
+
+-- corporatesテーブルを削除（存在する場合）
+DROP TABLE IF EXISTS corporates;
+-- corporatesテーブルを作成
+CREATE TABLE corporates (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT,
+    intro TEXT,
+    image_path VARCHAR(255) NOT NULL, 
+    item_num INTEGER
+);
+
+INSERT INTO corporates (company, intro, image_path, item_num) 
+VALUES ('マクドナルド', 'チキンクリスプ2個ください', 'https://www.mcdonalds.co.jp/assets/images/mcd-logo.svg', (SELECT COUNT(*) FROM items WHERE items.company = 'マクドナルド'));
+INSERT INTO corporates (company, intro, image_path, item_num) 
+VALUES ('スターバックス', 'チキンクリスプ1個ください', 'https://www.mcdonalds.co.jp/assets/images/mcd-logo.svg', (SELECT COUNT(*) FROM items WHERE items.company = 'スターバックス'));
+INSERT INTO corporates (company, intro, image_path, item_num) 
+VALUES ('セブンイレブン', 'チキンクリスプ1個ください', 'https://www.mcdonalds.co.jp/assets/images/mcd-logo.svg', (SELECT COUNT(*) FROM items WHERE items.company = 'セブンイレブン'));
+INSERT INTO corporates (company, intro, image_path, item_num) 
+VALUES ('ローソン', 'チキンクリスプ1個ください', 'https://www.mcdonalds.co.jp/assets/images/mcd-logo.svg', (SELECT COUNT(*) FROM items WHERE items.company = 'ローソン'));
+INSERT INTO corporates (company, intro, image_path, item_num) 
+VALUES ('ファミリーマート', 'チキンクリスプ1個ください', 'https://www.mcdonalds.co.jp/assets/images/mcd-logo.svg', (SELECT COUNT(*) FROM items WHERE items.company = 'ファミリーマート'));
+
+SELECT * FROM corporates;
