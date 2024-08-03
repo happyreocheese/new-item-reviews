b660c98f4539e2fb2f5cd04e0b25ad1920a2d664
tayuuki <yuuki.tatsu848@gmail.com>
2024-08-04 02:14:40 +0900

    App.jsxの更新

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 import Header from "./components/Header";
+import Company from "./pages/Company";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,97 +26,18 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        <ul>
-          {array.length > 0 ? (
-            array.map((name, index) => <li key={index}>{name}</li>)
-          ) : (
-            <li>Loading...</li>
-          )}
-        </ul>
-      </main>
-
-      <body>
-        <div className="container flex md:ml-auto">
-          <section>
-            <div className="border-solid border-8">
-              <div className="font-medium text-xl px-20 pt-5">
-                今のおすすめ
-              </div>
-              <nav className="flex flex-col mx-auto p-5">
-                <li><a href="#" value="1">商品名</a></li>
-                <li><a href="#" value="2">商品名</a></li>
-                <li><a href="#" value="3">商品名</a></li>
-                <li><a href="#" value="4">商品名</a></li>
-                <li><a href="#" value="5">商品名</a></li>
-              </nav>
-            </div>
-          </section>
-          <section>
-
-          <div className="flex">
-            <div>
-              <img src="./img.jpg" alt="a"></img>
-            </div>
-            <div>
-              <img src="./img.jpg" alt="a"></img>
-            </div>
-            <div>
-              <img src="./img.jpg" alt="a"></img>
-            </div>
-            <div>
-              <img src="./img.jpg" alt="a"></img>
-            </div>
-            <div>
-              <img src="./img.jpg" alt="b"></img>
-            </div>
-          </div>
-
-          </section>
-
-         
-          <section className="justify-center">
-            <ul className="flex flex-wrap border-solid border-8 justify-center">
-              <li className="basis-2/5 text-center px-5 py-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
-                <a href="https://example.com">
-                  <img
-                    src="./img/mac.jpg"
-                    alt="mac"
-                    className="w-28 mx-auto"
-                  ></img>
-                </a>
-              </li>
-              <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
-                <a href="https://example.com">
-                  <img
-                    src="./img/mac.jpg"
-                    alt="mac"
-                    className="w-28 mx-auto"
-                  ></img>
-                </a>
-              </li>
-              <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
-                <a href="https://example.com">
-                  <img
-                    src="./img/mac.jpg"
-                    alt="mac"
-                    className="w-28 mx-auto"
-                  ></img>
-                </a>
-              </li>
-              <li className="basis-2/5 text-center px-5 pt-5 m-1.5 border-solid border-8 border-teal-400 rounded-2xl">
-                <a href="https://example.com">
-                  <img
-                    src="./img/mac.jpg"
-                    alt="mac"
-                    className="w-28 mx-auto"
-                  ></img>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
-      </body>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/company' element={
+            <Company
+              companyName="スターバックス"
+              stringP="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aut tempore necessitatibus odio? Omnis, voluptas aperiam numquam, eaque architecto laboriosam nisi expedita enim provident placeat alias ullam officia accusamus distinctio?"
+              />
+          }/>
+          <Route path='/product' element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
